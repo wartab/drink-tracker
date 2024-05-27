40e58d5c44fc6d9b2cda0ed9b3fcd3576b068a26
Vincent Lycoops <wartab@users.noreply.github.com>
2024-05-27 15:58:05 +0200

    refactor: Use lazy loading for routes (#5)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,19 +1,30 @@
 import {Routes} from "@angular/router";
 import {AuthenticationGuard} from "../authentication.service";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {LeaderboardComponent} from "./leaderboard/leaderboard.component";
-import {LoginComponent} from "./login/login.component";
-import {MainComponent} from "./main/main.component";
-import {RegisterComponent} from "./register/register.component";
-import {UserProfileComponent} from "./user-profile/user-profile.component";
 
 export const routes: Routes = [
-    {path: "", component: LoginComponent, pathMatch: "full"},
-    {path: "", component: MainComponent, children: [
-        {path: "main", component: MainComponent, pathMatch: "full"},
-        {path: "leaderboard", component: LeaderboardComponent, pathMatch: "full"},
-        {path: "dashboard", component: DashboardComponent, pathMatch: "full"},
-        {path: "user/:id", component: UserProfileComponent, pathMatch: "full"},
-    ], canActivate: [AuthenticationGuard]},
-    {path: "register", component: RegisterComponent, pathMatch: "full"},
+    {path: "", loadComponent: () => import("./login/login.component").then(c => c.LoginComponent), pathMatch: "full"},
+    {
+        path: "", loadComponent: () => import("./main/main.component").then(c => c.MainComponent), children: [
+            {
+                path: "leaderboard",
+                loadComponent: () => import("./leaderboard/leaderboard.component").then(c => c.LeaderboardComponent),
+                pathMatch: "full",
+            },
+            {
+                path: "dashboard",
+                loadComponent: () => import("./dashboard/dashboard.component").then(c => c.DashboardComponent),
+                pathMatch: "full",
+            },
+            {
+                path: "user/:id",
+                loadComponent: () => import("./user-profile/user-profile.component").then(c => c.UserProfileComponent),
+                pathMatch: "full",
+            },
+        ], canActivate: [AuthenticationGuard],
+    },
+    {
+        path: "register",
+        loadComponent: () => import("./register/register.component").then(c => c.RegisterComponent),
+        pathMatch: "full",
+    },
 ];
